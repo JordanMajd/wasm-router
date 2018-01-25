ece2756a37569ff7462c42b48f73bdec19a2bae9
Jordan Majd <jordan@madtitanstudios.com>
2018-01-25 00:06:02 -0700

    Correct typo in script causing wasm to not build.

--- makewasm.sh
+++ makewasm.sh
@@ -6,6 +6,6 @@ RELEASE="release"
 ARGS="+nightly build --$RELEASE --target $TARGET --manifest-path=$DIR/lib/rust/Cargo.toml"
 
 if cargo $ARGS; then
-  FILE="$DIR/lib/rust/target/$TARGET/$RELEASE/router.wasm"
+  FILE="$DIR/lib/rust/target/$TARGET/$RELEASE/wasm_router.wasm"
   [ -f $FILE ] && cp -r $FILE "$DIR/lib/js/router.wasm"
 fi
